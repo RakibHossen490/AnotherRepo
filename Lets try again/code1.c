605727e51f00643ecae3cdf94f5a2be58d822893
RakibHossen490 <mdrakibhossenhowladar@gmail.com>
2024-11-26 20:38:56 +0600

    Code Update

--- Lets try again/code1.c
+++ Lets try again/code1.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     printf("Enter THe value : ");
-    scanf("%d",&n);
-    for(int i=0; i<n; i++){
-        printf("%d ",i);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", i);
     }
+    printf("\nEnd Of Code");
     return 0;
 }
